64f9b1954a7ddb5854b7339544f29b231e93bebb
Pedro Mello <pedroadmn@gmail.com>
2025-01-11 16:26:42 -0300

    configure jest. remove karma/jasmine. add scripts to run tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,8 +24,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Hello, aceplayer',
+    expect(compiled.querySelector('div')?.textContent).toContain(
+      'Primeiro Deploy do Projeto',
     );
   });
 });
